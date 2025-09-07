8d63b265fa78a7b41596801e2bd9d4933393291c
EstebanCarrilloG <estexav0996@gmail.com>
2025-09-06 19:29:57 -0500

    JavaScript Trivia Bot

---
+++ JavaScript/labs/javaScript-trivia-bot/index.js
@@ -0,0 +1,20 @@
+console.log("Hello! I'm your coding fun fact guide!");
+let botName = "Pepe";
+let botLocation = "Hawai";
+let favoriteLanguage = "JavaScript";
+
+console.log(`My name is ${botName} and I live on ${botLocation}.`);
+console.log(`My favorite programming language is ${favoriteLanguage}.`);
+
+let codingFact = "- " + favoriteLanguage + "has no relation to java.";
+console.log(codingFact);
+
+codingFact = "- " + favoriteLanguage + " was build in 10 days.";
+console.log(codingFact);
+
+codingFact = "- In " + favoriteLanguage + " => is called a 'fat arrow'.";
+console.log(codingFact);
+
+console.log(
+  `It was fun sharing these facts with you. Goodbye! - ${botName} from ${botLocation}.`
+);
